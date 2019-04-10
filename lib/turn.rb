4a85d5752687777ac3819eccad884f84e5bcd185
Bruce Ruffin <bruffin1221@gmail.com>
2019-04-10 05:02:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,5 +34,6 @@ def turn(board)
    index=input_to_index(user_input)
    valid_move?(board, index)
    move(board, index, value = "X")
+    position_taken?(board, index)
   display_board(board)
  end
