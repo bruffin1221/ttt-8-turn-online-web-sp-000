8588ffec6581b5284817da31fd2bb54c9e4e3852
Bruce Ruffin <bruffin1221@gmail.com>
2019-04-10 05:11:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,6 +33,7 @@ def turn(board)
    user_input = gets.strip
    index=input_to_index(user_input)
    valid_move?(board, index)
+   turn(board)
    move(board, index, value = "X")
   display_board(board)
  end
